e3cbc4ba86d4bc2b8347307fd234113973ad278f
thedorktrain <mackemosabe@gmail.com>
2024-06-11 23:18:30 -0500

    adding header and background some base css elements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import backgroundImage1 from './assets/background_maps/map1.jpg';
+import backgroundImage2 from './assets/background_maps/map2.jpg';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [randomImage, setRandomImage] = useState('');
+
+  useEffect(() => {
+    const backgroundImages = [backgroundImage1, backgroundImage2];
+    const randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
+    setRandomImage(randomImage);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section id="mainSection" style={{ backgroundImage: `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url(${randomImage})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', minHeight: '100vh', backgroundPosition: 'center', }}>
+        <div class= "box">
+          <p>Welcome to Adventure Vault!</p></div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

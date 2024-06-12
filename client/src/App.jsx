323a578b09df4b336d2f4a99a1b94b92408da1a7
erikaylopez <eylopez8686@gmail.com>
2024-06-11 18:33:50 -0600

    created pages folder and files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,30 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() { 
+  const [count, setCount] = useState(0) 
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
+      <h1>Adventurer Vault</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button className="btn btn-lg btn-info m-2" to="/login">
+          Login
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button className="btn btn-lg btn-light m-2" to="/signup">
+          Signup
+        </button>
       </div>
+
+      
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      
       </p>
     </>
   )
